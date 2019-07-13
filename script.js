3aa4aad1e9a9c6005116e36ea44000e951717336
reenaishtiaq <49521474+reenaishtiaq@users.noreply.github.com>
2019-07-13 17:16:00 +0500

    WIP

---
+++ script.js
@@ -0,0 +1,310 @@
+let pc = {
+    // used json
+    // used json of string
+
+    resturantName: "Pearl Continental",
+    resturantType: "all type of eatable items",
+    courseDescription: "Tech Karo is an initiative to prevent marginalization of women in the tech community.",
+    address: "Club Rd, Civil Lines, Karachi, Karachi City, Sindh",
+    phone: "(021) 111 505 505",
+    location: "4.3 Near the city center; sightseeing nearby·Guests noted the affordable parking",
+    serviceAndFacilitiesRates: 4.3,
+    isMasjid: true,
+
+    hotelFeatures: ["Health Club", "Free WiFi", "Business Center", "Swimming Pool", "Free Under Ground Parking Area"],
+    deluxRooms: {
+        isRooms: true,
+        bedType: "2 twin beds and 1 queen bed",
+        provider: "Agoda",
+        rates: "83$",
+
+        bedType: "2 twin beds and 1 queen bed",
+        provider: "Expedia",
+        rates: "100$",
+    },
+
+    kingRooms: {
+        isRooms: true,
+        bedType: "1 king bed",
+        provider: "Hotel websites",
+        rates: "115$",
+
+        bedType: "1 king bed",
+        provider: "booking.com",
+        rates: "115$",
+    },
+
+    queenRooms: {
+        isRooms: true,
+        bedType: "1 queen bed",
+        provider: "Hotel websites",
+        rates: "200$",
+
+        bedType: "2 queen bed",
+        provider: "booking.com",
+        rates: "250$",
+    },
+
+    standardRooms: {
+        isRooms: true,
+        bedType: "2 twin bed",
+        provider: "Hotel websites",
+        rates: "100$",
+
+        bedType: "2 twin bed",
+        provider: "booking.com",
+        rates: "225$",
+
+        bedType: "2 twin bed",
+        provider: "Aroma",
+        rates: "208$"
+    },
+
+    executiveRooms: {
+        isRooms: true,
+        bedType: "1 queen 1 double bed",
+        provider: "expedia",
+        rates: "130$",
+
+        bedType: "1 queen 1 double bed 1 king",
+        provider: "booking.com",
+        rates: "225$",
+
+        bedType: "1 queen 1 double 1 king bed",
+        provider: "hotwire",
+        rates: "208$"
+    },
+
+    roomFacility: {
+        isHomeComforts: true,
+        inRoomClimateControl: "(air conditioning)Air conditioning Minibar Bath Room Slippers",
+        sleepWell: true,
+        isbblackout: true,
+        beEntertained: true,
+        tv: "42-inch LCD TV Cable TV channels",
+        isWiFi: true,
+        isFoodaAndDrink: true,
+        isFreeBottledWater: true,
+        isDailyHousekeepingInRoomSafe: true,
+    },
+    hiTea: {
+        nameOfTea: "MARCO POLO",
+        time: "16:00-19:00",
+
+        nameOfTea: "TAI-PAN",
+        time: "19:30-23:30",
+
+        nameOfTea: "CHANDNI",
+        time: "16:00-19:00",
+
+        nameOfTea: "CHANDNI",
+        time: "16:00-19:00",
+
+        nameOfTea: "SAKURA",
+        time: "19:00-24:00",
+
+        nameOfTea: "JASON'S STEAK HOUSE",
+        time: "19:30-25:30",
+
+        nameOfTea: "GRAPE-VINE",
+        time: "16:00-26:00",
+    },
+    banquet: {
+        nameOfBanquet: ["Shalimar",
+            "Dilkusha",
+            "Henna",
+            "Kohinoor",
+            "Mahnoor",
+            "Emerald",
+            "Zaver",
+            "Marquee",
+            "Tulip",
+            "Carnation",
+            "Orchid"
+        ],
+        securityFeatures: [
+            "cctv",
+            "walk through gate",
+            " body scanners",
+            "fire fighting equipment",
+            "sniffer dogs",
+            "Well-trained security staff"
+        ],
+        capacity: "1500 people",
+    },
+    meetingAndConferences: {
+        placeOfMeeting: [
+            "Shopping arcade",
+            "Steak House",
+            "Executive Floors",
+            "Board Rooms"
+        ],
+        securityFeatures: [
+            "cctv",
+            "walk through gate",
+            " body scanners",
+            "fire fighting equipment",
+            "sniffer dogs",
+            "Well-trained security staff"
+        ],
+    },
+    deals: [{
+            noOfDeal: 1,
+            items: "Biryani small, chicken chatni roll & 250ml soft drink",
+            rates: "PKR 3100.00"
+        },
+
+
+        {
+            noOfDeal: 2,
+            items: "Singaporean rice small, chicken chatni roll & two 250ml soft drinks",
+            rates: "PKR 3000.00"
+        },
+
+        {
+            noOfDeal: 3,
+            items: "Biryani small, chicken chatni roll & 100ml soft drink",
+            rates: "PKR 3500.00",
+        },
+        {
+            noOfDeal: 4,
+            items: "rice small, chicken chatni roll & 250ml soft drink",
+            rates: "PKR 2000.00"
+        }
+    ],
+    Location: "clifton road, near ocean mall",
+    howToGet: {
+        byAir: "At a distance of 17 KM from Jinnah International Airport Karachi",
+        byTrain: "At a distance of 2 KM from the main railway station",
+        byRoad: "Pearl Continental Karachi is situated on Club Road, Karachi"
+    },
+    breafast: {
+        nameOfitems: "THE HOG & CHICK",
+        rates: 3000,
+
+        nameOfitems: "salad",
+        rates: 1000,
+
+        nameOfitems: "THE HOG & CHICK",
+        rates: 3000,
+
+        nameOfitems: "sandwich",
+        rates: 1500,
+
+        nameOfitems: "steak",
+        rates: 2000,
+
+        nameOfitems: "tuna steak",
+        rates: 2500,
+
+        nameOfitems: "fish",
+        rates: "1000-4000",
+
+        nameOfitems: "rice",
+        rates: 3500,
+
+        nameOfitems: "spaghetti",
+        rates: 1100,
+
+        nameOfitems: "spaghetti",
+        rates: 1100,
+
+        nameOfitems: "pizza",
+        rates: "1100-4000",
+
+        nameOfitems: "sausages",
+        rates: 1000,
+
+        nameOfitems: "spaghetti",
+        rates: 1100
+    },
+    jucies: {
+        nameOfJucie: "apple juice",
+        rates: 500,
+
+        nameOfJucie: "banana juice",
+        rates: 700,
+
+        nameOfJucie: "mango juice",
+        rates: 1000,
+
+        nameOfJucie: "grapes juice",
+        rates: 800,
+
+        nameOfJucie: "milk",
+        rates: 500,
+    },
+    deserts: {
+        name: "pie",
+        rates: 1500,
+
+        name: "cup cake",
+        rates: 1800,
+
+        name: "Buttery Raspberry Crumble Bars.",
+        rates: 2000,
+
+        name: "Mint Oreo Cake",
+        rates: 3500,
+
+        name: "Ultimate Gooey Brownies",
+        rates: 2500,
+
+        name: "Chocolate And Strawberry Cake",
+        rates: 2000,
+
+        name: "Old Fashioned Sour Cream Doughnuts",
+        rates: 4500,
+
+        name: "Coconut Cream Crepe Cake",
+        rates: "1200-1900",
+
+        name: "Coconut Cream Crepe Cake",
+        rates: 2300,
+
+        name: "Meyer Lemon Bars.",
+        rates: 1500,
+    },
+    lunch: {
+        nameOfItem: "Tangy Noodle Salad",
+        rates: 2000,
+
+        nameOfItem: "Sushi Pockets",
+        rates: 1000,
+
+        nameOfItem: "Guacamole",
+        rates: 2500,
+
+        nameOfItem: "Egg & Arugula on Crackers.",
+        rates: "700-1500",
+
+        nameOfItem: "Almond-Currant Couscous",
+        rates: 1700,
+
+        nameOfItem: "Grilled Chicken",
+        rates: 2800,
+
+        nameOfItem: "Aged Cheddar",
+        rates: 2700,
+
+        nameOfItem: "Chutney Sandwich",
+        rates: 1200,
+    },
+    branches: [
+        "Pearl Continental",
+        "Pearl Continental Lahore",
+        "Pearl Continental Rawalpindi",
+        "Pearl Continental Peshawar",
+        "Pearl Continental Burhban",
+        "Pearl Continental Muzaffarabad",
+        "Pearl Continental Gawadar",
+        "Pearl Continental Hyderabad",
+        "Pearl Continental Multan",
+        "Pearl Continental Mirpur",
+        "Pearl Continental Malam Jabba",
+    ],
+
+
+
+
+}
